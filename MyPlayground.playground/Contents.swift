32c651dcb5e26a0a649a7986d57921926296484b
Andronov Andrey <laserdo25@gmail.com>
2025-07-30 11:50:03 +0300

    fix Homework

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -6,6 +6,6 @@ let minutes: Double
 let hours: Double
 
 seconds = number
-minutes = Double(number) / 60
+minutes = Double(seconds) / 60
 hours = Double(minutes) / 60
-print("\(number) это \(seconds) секунд, \(minutes) минут, \(hours) часов")
+print("Число \(number) это \(seconds) секунд, \(minutes) минут, \(hours) часов")
