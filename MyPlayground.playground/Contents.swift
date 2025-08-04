cfe8bc32d12640ff23f5fa2ff3d55b684b4db21a
Andronov Andrey <laserdo25@gmail.com>
2025-08-04 19:32:23 +0300

    Fix Homework

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -2,10 +2,25 @@ import Foundation
 
 let number = 654321
 let seconds: Int
-let minutes: Double
-let hours: Double
+let minutes: Int
+let hours: Int
 
-seconds = number
-minutes = Double(seconds) / 60
-hours = Double(minutes) / 60
-print("Число \(number) это \(seconds) секунд, \(minutes) минут, \(hours) часов")
+hours = number / 3600
+minutes = (number % 3600) / 60
+seconds = number % 60
+
+print("\(number) это \(hours) часов, \(minutes) минут, \(seconds) секунд")
+
+
+//let number = 654321
+// Общее количество секунд, которое мы хотим перевести в часы, минуты и секунды
+
+//let hours = number / 3600
+// Считаем, сколько в этом числе полных часов (в одном часе 3600 секунд)
+
+//let minutes = (number % 3600) / 60
+// Остаток от деления на 3600 — это то, что не влезло в часы
+// Делим этот остаток на 60 — получаем количество полных минут
+
+//let seconds = number % 60
+// Считаем остаток от деления на 60 — это то, что не влезло в минуты, просто секунды
